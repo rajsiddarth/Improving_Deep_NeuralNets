de2360ee06174dbff9ba2938ce2cc7f312a4136a
Siddarth <rajasiddartha94@gmail.com>
2017-11-25 15:07:02 -0700

    Update reg_fns.py

--- Regularization/utility_fns/reg_fns.py
+++ Regularization/utility_fns/reg_fns.py
@@ -121,6 +121,31 @@ def forward_propagation(X, parameters):
     cache = (Z1, A1, W1, b1, Z2, A2, W2, b2, Z3, A3, W3, b3)
     
     return A3, cache
+def load_dataset():
+    train_dataset = h5py.File('train_data.h5', "r")
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
+    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
+
+    test_dataset = h5py.File('datasets/test_data.h5', "r")
+    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
+    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
+
+    classes = np.array(test_dataset["list_classes"][:]) # the list of classes
+    
+    train_set_y = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    test_set_y = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+    
+    train_set_x_orig = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
+    test_set_x_orig = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
+    
+    train_set_x = train_set_x_orig/255
+    test_set_x = test_set_x_orig/255
+
+    return train_set_x, train_set_y, test_set_x, test_set_y, classes
+
+
+
+
 
 def backward_propagation(X, Y, cache):
     """
@@ -296,14 +321,4 @@ def plot_decision_boundary(model, X, y):
     plt.xlabel('x1')
     plt.scatter(X[0, :], X[1, :], c=y, cmap=plt.cm.Spectral)
     plt.show()
-    
-def load_2D_dataset():
-    data = scipy.io.loadmat('datasets/data.mat')
-    train_X = data['X'].T
-    train_Y = data['y'].T
-    test_X = data['Xval'].T
-    test_Y = data['yval'].T
 
-    plt.scatter(train_X[0, :], train_X[1, :], c=train_Y, s=40, cmap=plt.cm.Spectral);
-    
-    return train_X, train_Y, test_X, test_Y
