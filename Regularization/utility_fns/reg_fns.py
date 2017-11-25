40f3766d8e645fc03971c847be2994790cf6950c
Siddarth <rajasiddartha94@gmail.com>
2017-11-25 15:03:56 -0700

    Update reg_fns.py

--- Regularization/utility_fns/reg_fns.py
+++ Regularization/utility_fns/reg_fns.py
@@ -234,29 +234,6 @@ def compute_cost(a3, Y):
     
     return cost
 
-def load_dataset():
-    train_dataset = h5py.File('datasets/train_catvnoncat.h5', "r")
-    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
-    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
-
-    test_dataset = h5py.File('datasets/test_catvnoncat.h5', "r")
-    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
-    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
-
-    classes = np.array(test_dataset["list_classes"][:]) # the list of classes
-    
-    train_set_y = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
-    test_set_y = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
-    
-    train_set_x_orig = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
-    test_set_x_orig = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
-    
-    train_set_x = train_set_x_orig/255
-    test_set_x = test_set_x_orig/255
-
-    return train_set_x, train_set_y, test_set_x, test_set_y, classes
-
-
 def predict_dec(parameters, X):
     """
     Used for plotting decision boundary.
